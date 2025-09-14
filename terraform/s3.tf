b8e7ed2bf535517a52ee32870c1fb407ea0d37b7
Kevin Della Piazza <kevindellapiazza1@gmail.com>
2025-09-15 00:20:13 +0200

    feat(infra): Define Bronze and Silver S3 buckets with Terraform

---
+++ terraform/s3.tf
@@ -0,0 +1,5 @@
+provider "aws" { region = "eu-west-1"}
+
+resource "aws_s3_bucket" "bronze_bucket" {bucket = "reviewlens-bronze-bucket-kevin"}
+
+resource "aws_s3_bucket" "silver_bucket" {bucket = "reviewlens-silver-bucket-kevin"}
