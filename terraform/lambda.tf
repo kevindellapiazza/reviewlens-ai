97a7f2190f10b54ecc86d37774eb3ee8f046a05e
Kevin Della Piazza <kevindellapiazza1@gmail.com>
2025-09-18 15:23:29 +0200

    refactor(infra): Rearchitect Lambda functions to Splitter, Processor, and Stitcher

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,114 +1,131 @@
-# --- ECR Repositories ---
-resource "aws_ecr_repository" "splitter_lambda_repo" {
-  name = "reviewlens-splitter-lambda-repo"
-}
-
+# --- ECR Repository ---
 resource "aws_ecr_repository" "processor_lambda_repo" {
   name = "reviewlens-processor-lambda-repo"
 }
-
-
-# --- IAM Role for the Splitter Lambda ---
-# This role only needs to read from S3 Bronze and write to SQS
-resource "aws_iam_role" "splitter_lambda_role" {
-  name = "reviewlens-splitter-lambda-role"
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = { Service = "lambda.amazonaws.com" }
-    }]
-  })
+resource "aws_ecr_repository" "stitcher_lambda_repo" {
+  name = "reviewlens-stitcher-lambda-repo"
 }
 
-# Attaches basic execution policy for logs
+# --- IAM Roles and Policies (Separate for each function) ---
+
+# --- LAMBDA SPLITTER ROLE ---
+resource "aws_iam_role" "splitter_lambda_role" {
+  name               = "reviewlens-splitter-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
 resource "aws_iam_role_policy_attachment" "splitter_basic_execution" {
   role       = aws_iam_role.splitter_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-# Custom policy for the Splitter
+resource "aws_iam_role_policy_attachment" "splitter_policy_attachment" {
+  role       = aws_iam_role.splitter_lambda_role.name
+  policy_arn = aws_iam_policy.splitter_policy.arn
+}
 resource "aws_iam_policy" "splitter_policy" {
   name   = "reviewlens-splitter-permissions"
   policy = jsonencode({
     Version   = "2012-10-17",
     Statement = [
-      {
-        Action   = ["s3:GetObject"],
-        Effect   = "Allow",
-        Resource = ["${aws_s3_bucket.bronze_bucket.arn}/*"]
-      },
-      {
-        Action   = ["sqs:SendMessage"],
-        Effect   = "Allow",
-        Resource = [aws_sqs_queue.reviews_queue.arn]
-      }
+      { Action = ["s3:GetObject"], Effect = "Allow", Resource = ["${aws_s3_bucket.bronze_bucket.arn}/*"] },
+      { Action = ["sqs:SendMessage"], Effect = "Allow", Resource = [aws_sqs_queue.reviews_queue.arn] }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "splitter_policy_attachment" {
-  role       = aws_iam_role.splitter_lambda_role.name
-  policy_arn = aws_iam_policy.splitter_policy.arn
-}
-
-
-# --- IAM Role for the Processor Lambda ---
-# This role now needs to read from SQS and write to S3 Silver
+# --- LAMBDA PROCESSOR ROLE ---
 resource "aws_iam_role" "processor_lambda_role" {
-  name               = "reviewlens-processor-lambda-role" # Renamed for clarity
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = { Service = "lambda.amazonaws.com" }
-    }]
-  })
+  name               = "reviewlens-processor-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
-
 resource "aws_iam_role_policy_attachment" "processor_basic_execution" {
   role       = aws_iam_role.processor_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
+resource "aws_iam_role_policy_attachment" "processor_policy_attachment" {
+  role       = aws_iam_role.processor_lambda_role.name
+  policy_arn = aws_iam_policy.processor_policy.arn
+}
 resource "aws_iam_policy" "processor_policy" {
   name   = "reviewlens-processor-permissions"
   policy = jsonencode({
     Version   = "2012-10-17",
     Statement = [
-      {
-        Action   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"],
-        Effect   = "Allow",
-        Resource = [aws_sqs_queue.reviews_queue.arn]
-      },
-      {
-        Action   = ["s3:PutObject"],
-        Effect   = "Allow",
-        Resource = ["${aws_s3_bucket.silver_bucket.arn}/*"]
-      }
+      { Action = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"], Effect = "Allow", Resource = [aws_sqs_queue.reviews_queue.arn] },
+      { Action = ["s3:PutObject"], Effect = "Allow", Resource = ["${aws_s3_bucket.silver_bucket.arn}/*"] }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "processor_policy_attachment" {
-  role       = aws_iam_role.processor_lambda_role.name
-  policy_arn = aws_iam_policy.processor_policy.arn
+# --- LAMBDA STITCHER ROLE ---
+resource "aws_iam_role" "stitcher_lambda_role" {
+  name               = "reviewlens-stitcher-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+resource "aws_iam_role_policy_attachment" "stitcher_basic_execution" {
+  role       = aws_iam_role.stitcher_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+resource "aws_iam_role_policy_attachment" "stitcher_policy_attachment" {
+  role       = aws_iam_role.stitcher_lambda_role.name
+  policy_arn = aws_iam_policy.stitcher_policy.arn
+}
+resource "aws_iam_policy" "stitcher_policy" {
+  name   = "reviewlens-stitcher-permissions"
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      { Action = ["s3:GetObject", "s3:ListBucket", "s3:DeleteObject"], Effect = "Allow", Resource = [aws_s3_bucket.silver_bucket.arn, "${aws_s3_bucket.silver_bucket.arn}/*"] },
+      { Action = ["s3:PutObject"], Effect = "Allow", Resource = ["${aws_s3_bucket.gold_bucket.arn}/*"] }
+    ]
+  })
 }
 
+# --- Data source for Lambda assume role policy ---
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
 
-# --- Lambda Function #1: The Splitter ---
+# --- ZIP Archive Creation and Upload to S3 ---
+data "archive_file" "splitter_zip" {
+  type        = "zip"
+  source_dir  = "../src/splitter_lambda/"
+  output_path = "../dist/splitter_lambda.zip"
+}
+resource "aws_s3_object" "splitter_zip_object" {
+  bucket = aws_s3_bucket.lambda_code_bucket.id
+  key    = "splitter_lambda.zip"
+  source = data.archive_file.splitter_zip.output_path
+  etag   = data.archive_file.splitter_zip.output_md5
+}
+
+# --- Lambda Function #1: The Splitter (from S3 .zip) ---
 resource "aws_lambda_function" "splitter_lambda" {
   function_name = "reviewlens-splitter-lambda"
   role          = aws_iam_role.splitter_lambda_role.arn
-  package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.splitter_lambda_repo.repository_url}:latest"
-  timeout       = 120 # 2 minutes is plenty to split a large file
-  memory_size   = 512
+  package_type  = "Zip"
+  handler       = "main.handler"
+  runtime       = "python3.9"
+
+  s3_bucket        = aws_s3_bucket.lambda_code_bucket.id
+  s3_key           = aws_s3_object.splitter_zip_object.key
+  source_code_hash = data.archive_file.splitter_zip.output_base64sha256
+
+  timeout     = 120
+  memory_size = 512
+  environment {
+    variables = {
+      SQS_QUEUE_URL = aws_sqs_queue.reviews_queue.url
+    }
+  }
 }
 
-# --- Lambda Function #2: The Processor ---
+# --- Lambda Function #2: The Processor (using Docker) ---
 resource "aws_lambda_function" "processor_lambda" {
   function_name = "reviewlens-processor-lambda"
   role          = aws_iam_role.processor_lambda_role.arn
@@ -116,17 +133,32 @@ resource "aws_lambda_function" "processor_lambda" {
   image_uri     = "${aws_ecr_repository.processor_lambda_repo.repository_url}:latest"
   timeout       = 900
   memory_size   = 3008
-
   environment {
     variables = {
-      HF_HOME = "/tmp/huggingface_cache"
+      HF_HOME            = "/tmp/huggingface_cache"
       SILVER_BUCKET_NAME = aws_s3_bucket.silver_bucket.bucket
     }
   }
 }
 
+# --- Lambda Function #3: The Stitcher (using Docker) ---
+resource "aws_lambda_function" "stitcher_lambda" {
+  function_name = "reviewlens-stitcher-lambda"
+  role          = aws_iam_role.stitcher_lambda_role.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.stitcher_lambda_repo.repository_url}:latest" # <--- Modificato
 
-# --- Trigger #1: S3 to Splitter Lambda ---
+  timeout     = 300
+  memory_size = 2048
+  environment {
+    variables = {
+      SILVER_BUCKET_NAME = aws_s3_bucket.silver_bucket.bucket
+      GOLD_BUCKET_NAME   = aws_s3_bucket.gold_bucket.bucket
+    }
+  }
+}
+
+# --- Triggers ---
 resource "aws_s3_bucket_notification" "bronze_to_splitter_notification" {
   bucket = aws_s3_bucket.bronze_bucket.id
   lambda_function {
@@ -136,7 +168,6 @@ resource "aws_s3_bucket_notification" "bronze_to_splitter_notification" {
   }
   depends_on = [aws_lambda_permission.allow_s3_to_invoke_splitter]
 }
-
 resource "aws_lambda_permission" "allow_s3_to_invoke_splitter" {
   statement_id  = "AllowS3InvokeSplitter"
   action        = "lambda:InvokeFunction"
@@ -145,9 +176,21 @@ resource "aws_lambda_permission" "allow_s3_to_invoke_splitter" {
   source_arn    = aws_s3_bucket.bronze_bucket.arn
 }
 
-# --- Trigger #2: SQS to Processor Lambda ---
 resource "aws_lambda_event_source_mapping" "sqs_to_processor_trigger" {
   event_source_arn = aws_sqs_queue.reviews_queue.arn
   function_name    = aws_lambda_function.processor_lambda.arn
-  batch_size       = 1 # Process one message (one batch of reviews) at a time
+  batch_size       = 1
+}
+
+resource "aws_sns_topic_subscription" "stitcher_sns_subscription" {
+  topic_arn = aws_sns_topic.stitching_topic.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.stitcher_lambda.arn
+}
+resource "aws_lambda_permission" "allow_sns_to_invoke_stitcher" {
+  statement_id  = "AllowSNSInvokeStitcher"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.stitcher_lambda.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.stitching_topic.arn
 }
